7a8ad12e8f93c1f456a5ce168270c2b64b81c16a
Strikers47 <sidsat477501@gmail.com>
2020-09-22 22:59:37 +0530

    v-1.1.0

--- To do list/todolist.js
+++ To do list/todolist.js
@@ -1,29 +1,23 @@
 var list = document.getElementById('list');
 var __task;
 
-
+//zap_marker_infinity
 function  Task (task)  {
     console.log(task)
     this.date = new Date().toString().split(' ');
     var div  = document.createElement("div");
     var p = document.createElement("p");
-   
-       
-    
-            div.classList.add('task_div')
-            console.log(this.date)
-            var p = document.createElement("p");
-            
-            p.innerHTML =  this.date[0] + "," + this.date[1] + " " + this.date[2] + " " + this.date[3] + "<br>" + this.date[4] + "<br>" + task;
-            
-            div.appendChild(p);
-            
-            p.classList.add('tasks_');
-            
-           
-           
-        
-            list.appendChild(div);
+    var l = document.createElement("div");
+    l.innerHTML="<strong>X</strong>"
+    l.classList.add('button');
+    p.appendChild(l);
+    div.classList.add('task_div')
+    console.log(this.date)
+    var p = document.createElement("p");
+    p.innerHTML =  this.date[0] + "," + this.date[1] + " " + this.date[2] + " " + this.date[3] + "<br>" + this.date[4] + "<br>" + task;
+    div.appendChild(p);
+    p.classList.add('tasks_');  
+    list.appendChild(div);
     }
      
 
